6699bfccf3b693c9fa5c8ce000ef7759f89656e9
Duje Marušić <duje.marusic@q-software.com>
2020-02-13 11:45:56 +0100

    Ionic, firebase setup and login-register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,28 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import firebaseConfig from './environments/firebase';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { UserService } from './services/user.service';
+import 'firebase/firestore';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    UserService,
+    AngularFirestore
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app'
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  email: string;
+  password: string;
+  constructor(
+    private auth: AngularFireAuth,
+    private router: Router,
+    private user: UserService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  async login() {
+    const { email, password } = this;
+    try {
+      const res = await this.auth.signInWithEmailAndPassword(email, password)
+      if (res.user) {
+        this.user.setUser({
+          email,
+          uid: res.user.uid
+        });
+        this.router.navigateByUrl('tabs')
+      }
+    } catch (error) {
+      console.dir(error)
+    }
+  }
+
+}
